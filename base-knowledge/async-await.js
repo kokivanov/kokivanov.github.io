6cca7200437b38852f963a9f6d9aa86c8993223a
Maksym Dmytryshyn <maksym.dmytryshyn@globallogic.com>
2023-08-07 14:10:39 +0300

    async/await, errors, canvas

---
+++ base-knowledge/async-await.js
@@ -0,0 +1,17 @@
+/* Call async from non-async
+We have a “regular” function called f. How can you call the async function wait() and use its result inside of f? */
+async function wait() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  // ...what should you write here?
+  // we need to call async wait() and wait to get 10
+  // remember, we can't use "await"
+
+  wait().catch(err => {throw err}).then(resp => console.log(resp))
+}
+
+f()
